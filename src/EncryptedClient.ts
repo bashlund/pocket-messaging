ea8536575410d32739bceb73978e9015761bcb49
Thomas Backlund <bashlund@duck.com>
2024-02-11 11:20:03 +0100

    Refactor EncryptedClient.ts to extend pocket-sockets WrappedClient

--- src/EncryptedClient.ts
+++ src/EncryptedClient.ts
@@ -2,64 +2,41 @@ import {box, unbox, init} from "./Crypto";
 
 import {
     ClientInterface,
-    SocketErrorCallback,
     SocketDataCallback,
-    SocketConnectCallback,
-    SocketCloseCallback,
+    WrappedClient,
 } from "pocket-sockets";
 
 /**
  * Wrap an already connected and handshooked socket client as encrypted.
  *
  */
-export class EncryptedClient implements ClientInterface {
-    protected handlers: {[name: string]: ((args?: any) => void)[]} = {};
+export class EncryptedClient extends WrappedClient {
     protected incomingData: Buffer;
 
-    constructor(protected client: ClientInterface,
+    constructor(client: ClientInterface,
             protected outgoingKey: Buffer,
             protected outgoingNonce: Buffer,
             protected incomingKey: Buffer,
             protected incomingNonce: Buffer,
             protected peerPublicKey: Buffer) {
 
+        super(client);
+
         this.incomingData = Buffer.alloc(0);
     }
 
     public async init() {
+        await super.init();
+
         await init();  // Init sodium
     }
 
-    public getSocket(): any {
-        return this.client.getSocket();
-    }
-
-    public isWebSocket(): boolean {
-        return this.client.isWebSocket();
-    }
-
-    public isTextMode(): boolean {
-        return this.client.isTextMode();
-    }
-
-    public connect() {
-        throw new Error("The EncryptedSocket's underlaying socket should already have been connected");
-    }
-
     public unRead(data: Buffer) {  //eslint-disable-line @typescript-eslint/no-unused-vars
-        throw new Error("unRead function not available in EncryptedSocket");
+        throw new Error("unRead function not available in EncryptedClient");
     }
 
     public getPeerPublicKey(): Buffer {
         return this.peerPublicKey;
-    }
-
-    public close() {
-        this.client.close();
-    }
-
-    public isClosed(): boolean {
-        return this.client.isClosed();
     }
 
     protected decryptData() {
@@ -97,14 +74,6 @@ export class EncryptedClient implements ClientInterface {
         }
     }
 
-    public onError(fn: SocketErrorCallback) {
-        this.client.onError(fn);
-    }
-
-    public offError(fn: SocketErrorCallback) {
-        this.client.offError(fn);
-    }
-
     public onData(fn: SocketDataCallback) {
         this.hookEvent("data", fn);
 
@@ -130,58 +99,4 @@ export class EncryptedClient implements ClientInterface {
             throw new Error("EncryptedClient does not work with text data");
         }
     };
-
-    public onConnect(fn: SocketConnectCallback) {
-        this.client.onConnect(fn);
-    }
-
-    public offConnect(fn: SocketConnectCallback) {
-        this.client.offConnect(fn);
-    }
-
-    public onClose(fn: SocketCloseCallback) {
-        this.client.onClose(fn);
-    }
-
-    public offClose(fn: SocketCloseCallback) {
-        this.client.offClose(fn);
-    }
-
-    public getLocalAddress(): string | undefined {
-        return this.client.getLocalAddress();
-    }
-
-    public getRemoteAddress(): string | undefined {
-        return this.client.getRemoteAddress();
-    }
-
-    public getRemotePort(): number | undefined {
-        return this.client.getRemotePort();
-    }
-
-    public getLocalPort(): number | undefined {
-        return this.client.getLocalPort();
-    }
-
-    public getClient(): ClientInterface {
-        return this.client;
-    }
-
-    protected hookEvent(type: string, callback: (args?: any) => void) {
-        const cbs = this.handlers[type] || [];
-        this.handlers[type] = cbs;
-        cbs.push(callback);
-    }
-
-    protected unhookEvent(type: string, callback: (args?: any) => void) {
-        const cbs = (this.handlers[type] || []).filter( (cb: (args?: any) => void) => callback !== cb );
-        this.handlers[type] = cbs;
-    }
-
-    protected triggerEvent(type: string, ...args: any) {
-        const cbs = this.handlers[type] || [];
-        cbs.forEach( (callback) => {
-            callback(...args);
-        });
-    }
 }
